809e70e5748c8fddcb62318af1e6d057a923bdb4
陈勇 <dearchan@aliyun.com>
2025-03-13 17:19:23 +0800

    fix: 修改语音识别模型提升效率

--- videotrans/task/trans_create.py
+++ videotrans/task/trans_create.py
@@ -10,6 +10,7 @@ import time
 from pathlib import Path
 from typing import Dict
 from pydub import AudioSegment
+import requests
 from videotrans import translator
 from videotrans.configure import config
 from videotrans.recognition import run as run_recogn, Faster_Whisper_XXL
@@ -262,79 +263,97 @@ class TransCreate(BaseTask):
             )
 
             if self.cfg["recogn_type"] == Faster_Whisper_XXL:
+
                 import subprocess, shutil
 
-                cmd = [
-                    config.settings.get("Faster_Whisper_XXL", ""),
-                    self.cfg["shibie_audio"],
-                    "-f",
-                    "srt",
-                ]
-                if self.cfg["detect_language"] != "auto":
-                    cmd.extend(["-l", self.cfg["detect_language"][:2]])
-                cmd.extend(
-                    [
-                        "--model",
-                        self.cfg["model_name"],
-                        "--output_dir",
-                        self.cfg["target_dir"],
-                    ]
-                )
-                txt_file = (
-                        Path(
-                            config.settings.get("Faster_Whisper_XXL", "")
-                        ).parent.as_posix()
-                        + "/pyvideotrans.txt"
-                )
-                if Path(txt_file).exists():
-                    cmd.extend(
-                        Path(txt_file).read_text(encoding="utf-8").strip().split(" ")
-                    )
+                # cmd = [
+                #     config.settings.get("Faster_Whisper_XXL", ""),
+                #     self.cfg["shibie_audio"],
+                #     "-f",
+                #     "srt",
+                # ]
+                # if self.cfg["detect_language"] != "auto":
+                #     cmd.extend(["-l", self.cfg["detect_language"][:2]])
+                # cmd.extend(
+                #     [
+                #         "--model",
+                #         self.cfg["model_name"],
+                #         "--output_dir",
+                #         self.cfg["target_dir"],
+                #     ]
+                # )
+                # txt_file = (
+                #         Path(
+                #             config.settings.get("Faster_Whisper_XXL", "")
+                #         ).parent.as_posix()
+                #         + "/pyvideotrans.txt"
+                # )
+                # if Path(txt_file).exists():
+                #     cmd.extend(
+                #         Path(txt_file).read_text(encoding="utf-8").strip().split(" ")
+                #     )
 
-                while 1:
-                    if not config.copying:
-                        break
-                    time.sleep(1)
+                # while 1:
+                #     if not config.copying:
+                #         break
+                #     time.sleep(1)
 
-                subprocess.run(cmd)
-                # 定义输出的字幕文件路径
-                outsrt_file = (
-                        self.cfg["target_dir"]
-                        + "/"
-                        + Path(self.cfg["shibie_audio"]).stem
-                        + ".srt"
-                )
-                # 如果输出的字幕文件路径与源字幕路径不同，则复制到源字幕路径并删除原文件
-                if outsrt_file != self.cfg["source_sub"]:
-                    shutil.copy2(outsrt_file, self.cfg["source_sub"])
-                    Path(outsrt_file).unlink(missing_ok=True)
-                # 发送信号更新字幕内容
-                self._signal(
-                    text=Path(self.cfg["source_sub"]).read_text(encoding="utf-8"),
-                    type="replace_subtitle",
-                )
+                # subprocess.run(cmd)
+                # # 定义输出的字幕文件路径
+                # outsrt_file = (
+                #         self.cfg["target_dir"]
+                #         + "/"
+                #         + Path(self.cfg["shibie_audio"]).stem
+                #         + ".srt"
+                # )
+                # # 如果输出的字幕文件路径与源字幕路径不同，则复制到源字幕路径并删除原文件
+                # if outsrt_file != self.cfg["source_sub"]:
+                #     shutil.copy2(outsrt_file, self.cfg["source_sub"])
+                #     Path(outsrt_file).unlink(missing_ok=True)
+                # # 发送信号更新字幕内容
+                # self._signal(
+                #     text=Path(self.cfg["source_sub"]).read_text(encoding="utf-8"),
+                #     type="replace_subtitle",
+                # )
+
             else:
-                # 使用其他识别类型进行语音识别
+                # 使用其他识别模型进行语音识别
                 # raw_subtitles: 识别结果
-                raw_subtitles = run_recogn(
-                    # faster-whisper openai-whisper googlespeech
-                    recogn_type=self.cfg["recogn_type"],
-                    # 整体 预先 均等
-                    split_type=self.cfg["split_type"],
-                    uuid=self.uuid,
-                    # 模型名
-                    model_name=self.cfg["model_name"],
-                    # 识别音频
-                    audio_file=self.cfg["shibie_audio"],
-                    detect_language=self.cfg["detect_language"],
-                    cache_folder=self.cfg["cache_folder"],
-                    is_cuda=self.cfg["cuda"],
-                    subtitle_type=self.cfg.get("subtitle_type", 0),
-                    target_code=(
-                        self.cfg["target_language_code"] if self.shoud_trans else None
-                    ),
-                    inst=self,
-                )
+                split_audio_url = "http://127.0.0.1:10001/asr/client"
+                file_path = self.cfg["source_wav_output"]
+                with open(file_path, "rb") as file:
+                    files = {
+                        "file": file,
+                    }
+                    data = {
+                        "language": self.cfg["detect_language"]
+                    }
+                    response = requests.post(split_audio_url, files = files, data = data)
+                    if response.status_code == 200:
+                        raw_subtitles = response.json().get("data")
+                        self.source_srt_list = raw_subtitles
+                        self._save_srt_target(raw_subtitles, self.cfg["source_sub"])
+
+                # raw_subtitles = run_recogn(
+                #     # faster-whisper openai-whisper googlespeech
+                #     recogn_type=self.cfg["recogn_type"],
+                #     # 整体 预先 均等
+                #     split_type=self.cfg["split_type"],
+                #     uuid=self.uuid,
+                #     # 模型名
+                #     model_name=self.cfg["model_name"],
+                #     # 识别音频
+                #     audio_file=self.cfg["shibie_audio"],
+                #     detect_language=self.cfg["detect_language"],
+                #     cache_folder=self.cfg["cache_folder"],
+                #     is_cuda=self.cfg["cuda"],
+                #     subtitle_type=self.cfg.get("subtitle_type", 0),
+                #     target_code=(
+                #         self.cfg["target_language_code"] if self.shoud_trans else None
+                #     ),
+                #     inst=self,
+                # )
+
                 if self._exit():
                     return
                 # 没有识别到有效文字信息, 抛出异常
@@ -348,10 +367,12 @@ class TransCreate(BaseTask):
                 # 根据识别结果保存字幕文件
                 if isinstance(raw_subtitles, tuple):
                     self._save_srt_target(raw_subtitles[0], self.cfg["source_sub"])
+                    # 源视频字幕
                     self.source_srt_list = raw_subtitles[0]
                     if len(raw_subtitles) == 2:
                         self._save_srt_target(raw_subtitles[1], self.cfg["target_sub"])
                 else:
+                    #  raw_subtitles 源视频字幕文件
                     self._save_srt_target(raw_subtitles, self.cfg["source_sub"])
                     self.source_srt_list = raw_subtitles
             # 标记识别完成
@@ -461,6 +482,7 @@ class TransCreate(BaseTask):
             ):
                 if (
                         self.cfg["source_language_code"] != "auto"
+                        # zh-cn -> zh
                         and self.cfg["source_language_code"][:2]
                         not in config.ELEVENLABS_CLONE
                 ) or (
